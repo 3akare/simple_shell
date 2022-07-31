75b8853a81929a17dbd2519c0977945c571deea6
bakare <bakaredavid007@gmail.com>
2022-07-31 23:38:29 +0100

    added main.c and modified main.h

---
+++ main.c
@@ -0,0 +1,19 @@
+/**
+ * File: main.c
+ * Auth: Michael Rowland
+ * 	David Bakare
+ */
+
+#include "main.h"
+
+/**
+ * main - Runs a simple UNIX command interpreter
+ * @argc: The number of arguments supplied to the program
+ * @argv: An array of pointers to the arguments
+ *
+ * Return: success (0), fail (1)
+ */
+int main(int argc, char **argv)
+{
+	return (0);
+}
--- main.h
+++ main.h
@@ -1,7 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * File: main.h
+ * Auth: Michael Rowland
+ * 	David Bakare
+ */
+
+
 #include <stdio.h>
+#include <sys/stat.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
