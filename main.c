a5f40e57d9cc5609125f41b23cf4813b099c4e32
bakare <bakaredavid007@gmail.com>
2022-10-26 01:20:06 +0100

    code Refactor

--- main.c
+++ main.c
@@ -1,59 +1,134 @@
 #include "shell.h"
 
+void sig_handler(int sig);
+int execute(char **args, char **front);
+
 /**
- * non_interactive_mode - enables the shell to work in non-interactive mode
- *
- * @argv: an array of strings
- * Return: Always Successful
+ * sig_handler - Prints a new prompt upon a signal.
+ * @sig: The signal.
  */
-
-int non_interactive_mode(char **argv)
+void sig_handler(int sig)
 {
-	size_t size = 10;
-	pid_t child_pid;
-	int status, success = 0;
-	char *lineptr = malloc(100 * sizeof(char));
-	char new_lineptr[100];
-	char **arguments;
+	char *new_prompt = "\n$ ";
 
-	while (getline(&lineptr, &size, stdin) != -1)
-	{
-		if (strcmp(lineptr, "exit\n") == 0)
-		{
-			free(lineptr);
-			exit(0);
-		}
-		strcpy(new_lineptr, lineptr);
-		arguments = get_sh_tokens(new_lineptr);
-		if (arguments == NULL)
-			exit_error_non_interactive(argv[0], new_lineptr);
-		child_pid = fork();
-		if (child_pid == 0)
-		{
-			success = execvp(arguments[0], arguments);
-			if (success == -1)
-				exit_error_non_interactive(argv[0], new_lineptr);
-		}
-		else
-			wait(&status);
-	}
-	return (0);
+	(void)sig;
+	signal(SIGINT, sig_handler);
+	write(STDIN_FILENO, new_prompt, 3);
 }
 
+/**
+ * execute - Executes a command in a child process.
+ * @args: An array of arguments.
+ * @front: A double pointer to the beginning of args.
+ *
+ * Return: If an error occurs - a corresponding error code.
+ *         O/w - The exit value of the last executed command.
+ */
+int execute(char **args, char **front)
+{
+	pid_t child_pid;
+	int status, flag = 0, ret = 0;
+	char *command = args[0];
+
+	if (command[0] != '/' && command[0] != '.')
+	{
+		flag = 1;
+		command = get_location(command);
+	}
+
+	if (!command || (access(command, F_OK) == -1))
+	{
+		if (errno == EACCES)
+			ret = (create_error(args, 126));
+		else
+			ret = (create_error(args, 127));
+	}
+	else
+	{
+		child_pid = fork();
+		if (child_pid == -1)
+		{
+			if (flag)
+				free(command);
+			perror("Error child:");
+			return (1);
+		}
+		if (child_pid == 0)
+		{
+			execve(command, args, environ);
+			if (errno == EACCES)
+				ret = (create_error(args, 126));
+			free_env();
+			free_args(args, front);
+			free_alias_list(aliases);
+			_exit(ret);
+		}
+		else
+		{
+			wait(&status);
+			ret = WEXITSTATUS(status);
+		}
+	}
+	if (flag)
+		free(command);
+	return (ret);
+}
 
 /**
- * main - The main function of the shell
+ * main - Runs a simple UNIX command interpreter.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
  *
- * @ac: count of CMD line arguments vectors
- * @argv: an array of CMD line argument vectors
- * Return: Always Succesful
+ * Return: The return value of the last executed command.
  */
-
-int main(int __attribute__((unused)) ac, char *argv[])
+int main(int argc, char *argv[])
 {
+	int ret = 0, retn;
+	int *exe_ret = &retn;
+	char *prompt = "$ ", *new_line = "\n";
+
+	name = argv[0];
+	hist = 1;
+	aliases = NULL;
+	signal(SIGINT, sig_handler);
+
+	*exe_ret = 0;
+	environ = _copyenv();
+	if (!environ)
+		exit(-100);
+
+	if (argc != 1)
+	{
+		ret = proc_file_commands(argv[1], exe_ret);
+		free_env();
+		free_alias_list(aliases);
+		return (*exe_ret);
+	}
+
 	if (!isatty(STDIN_FILENO))
-		non_interactive_mode(argv);
-	else
-		shell_execute(argv);
-	return (0);
+	{
+		while (ret != END_OF_FILE && ret != EXIT)
+			ret = handle_args(exe_ret);
+		free_env();
+		free_alias_list(aliases);
+		return (*exe_ret);
+	}
+
+	while (1)
+	{
+		write(STDOUT_FILENO, prompt, 2);
+		ret = handle_args(exe_ret);
+		if (ret == END_OF_FILE || ret == EXIT)
+		{
+			if (ret == END_OF_FILE)
+				write(STDOUT_FILENO, new_line, 1);
+			free_env();
+			free_alias_list(aliases);
+			exit(*exe_ret);
+		}
+	}
+
+	free_env();
+	free_alias_list(aliases);
+	return (*exe_ret);
 }
